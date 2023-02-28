eec50b45416add0fa516a6fec709ff8ad50f512b
BeerHuntor <rt001a9138@gmail.com>
2023-02-28 14:44:30 +0000

    Noticed that the color backgrounds were not changing correctly - turns out that the color RGBA values range from 0-1 in Unity,  not 0-255 as first thought - Added code to divide value by 255 to show the correct color.

--- Assets/Scripts/DriverDisplayPanel.cs
+++ Assets/Scripts/DriverDisplayPanel.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
 using Color = UnityEngine.Color;
 
-public class DriverDisplayPanel : MonoBehaviour {
-    
+public class DriverDisplayPanel : MonoBehaviour
+{
+
     [SerializeField] private List<DriverSO> driverSoList;
     [SerializeField] private TextMeshProUGUI driverName;
     [SerializeField] private TextMeshProUGUI driverNumber;
@@ -15,41 +17,49 @@ public class DriverDisplayPanel : MonoBehaviour {
     [SerializeField] private UnityEngine.UI.Image driverNationality;
     [SerializeField] private UnityEngine.UI.Image backgroundColor;
 
-    [SerializeField] private int amountOfSpin = 6;
+    [SerializeField] private int amountOfSpin = 20;
     [SerializeField] private float timeBeforeNextSpin = 1f;
 
 
 
-    private void Update() {
+    private void Update()
+    {
         HandleInput();
 
     }
-    private void HandleInput() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             StartCoroutine(SelectDriver());
         }
     }
 
-    private IEnumerator SelectDriver() {
-            for(int i = 0; i < amountOfSpin; i++) {
-                DriverSO currentDriverSO = driverSoList[GetRandomDriverSOIndex()];
-                yield return StartCoroutine(DriverSpinAnimtion(currentDriverSO));
-                yield return new WaitForSeconds(timeBeforeNextSpin);
-            }
+    private IEnumerator SelectDriver()
+    {
+        for (int i = 0; i < amountOfSpin; i++)
+        {
+            DriverSO currentDriverSO = driverSoList[GetRandomDriverSOIndex()];
+            yield return StartCoroutine(DriverSpinAnimtion(currentDriverSO));
+            yield return new WaitForSeconds(timeBeforeNextSpin);
+        }
 
     }
 
-    private int GetRandomDriverSOIndex() {
+    private int GetRandomDriverSOIndex()
+    {
         int randomDriverSO = Random.Range(0, driverSoList.Count);
         return randomDriverSO;
     }
 
-    private IEnumerator DriverSpinAnimtion(DriverSO driverSo) {
-         ChangeDisplay(driverSo); 
+    private IEnumerator DriverSpinAnimtion(DriverSO driverSo)
+    {
+        ChangeDisplay(driverSo);
         yield return new WaitForSeconds(timeBeforeNextSpin);
     }
 
-    private void ChangeDisplay(DriverSO driverSo) {
+    private void ChangeDisplay(DriverSO driverSo)
+    {
         ChangeBackgroundColor(driverSo);
         this.driverName.SetText(driverSo.driverName);
         this.driverNumber.SetText(driverSo.driverNumber.ToString());
@@ -58,42 +68,46 @@ public class DriverDisplayPanel : MonoBehaviour {
         this.driverNationality.sprite = driverSo.driverNationality;
     }
 
-    private void ChangeBackgroundColor(DriverSO driverSo) {
-        switch(driverSo.team)
+    private void ChangeBackgroundColor(DriverSO driverSo)
+    {
+        switch (driverSo.team)
         {
             case "RedBull":
-                backgroundColor.color = new Color(31, 91, 197);
+                backgroundColor.color = new Color(DivideBy255(31), DivideBy255(91), DivideBy255(197));
                 break;
             case "Ferarri":
-                backgroundColor.color = new Color(231, 28, 35);
+                backgroundColor.color = new Color(DivideBy255(231), DivideBy255(28), DivideBy255(35));
                 break;
             case "Mercedes":
-                backgroundColor.color = new Color(0, 207, 186);
+                backgroundColor.color = new Color(DivideBy255(0), DivideBy255(207), DivideBy255(186));
                 break;
             case "Alpine":
-                backgroundColor.color = new Color(32, 139, 197);
+                backgroundColor.color = new Color(DivideBy255(32), DivideBy255(139), DivideBy255(197));
                 break;
             case "AlphaTauri":
-                backgroundColor.color = new Color(245, 241, 238);
+                backgroundColor.color = new Color(DivideBy255(76), DivideBy255(121), DivideBy255(152));
                 break;
             case "AlfaRomeo":
-                backgroundColor.color = new Color(177, 32, 56);
+                backgroundColor.color = new Color(DivideBy255(177), DivideBy255(32), DivideBy255(56));
                 break;
             case "Williams":
-                backgroundColor.color = new Color(54, 190, 221);
+                backgroundColor.color = new Color(DivideBy255(54), DivideBy255(190), DivideBy255(221));
                 break;
             case "Mclaren":
-                backgroundColor.color = new Color(234, 122, 31);
+                backgroundColor.color = new Color(DivideBy255(234), DivideBy255(122), DivideBy255(31));
                 break;
             case "AstonMartin":
-                backgroundColor.color = new Color(44, 129, 108);
+                backgroundColor.color = new Color(DivideBy255(44), DivideBy255(129), DivideBy255(108));
                 break;
             case "Haas":
-                backgroundColor.color = new Color(164, 168, 171);
+                backgroundColor.color = new Color(DivideBy255(164), DivideBy255(168), DivideBy255(171));
                 break;
         }
-
     }
 
+    private float DivideBy255(int RGBValueToDivide) {
 
+        return RGBValueToDivide / 255f; 
+
+    }
 }
